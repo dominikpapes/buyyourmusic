0f252449c2fc546d32ef5644e8c474f5439c3c32
MarinLovrinovic <lovrinovic.marin@gmail.com>
2025-05-25 16:57:35 +0200

    Make models.

--- bym/models.py
+++ bym/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    year = models.IntegerField()
+    price = models.FloatField()
+    cover = models.CharField(max_length=200)
+    rating = models.FloatField()
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.CharField(max_length=2000)
