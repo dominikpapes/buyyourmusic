dea90a50befd8f26bb204d90554f3698f20fc43b
dominik <dmnkpapes@gmail.com>
2025-05-25 16:08:48 +0200

    albums master/detail

--- bym/urls.py
+++ bym/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path('albums/<int:id>/', views.album_detail, name="album_detail")
 ]
